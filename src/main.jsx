f9d9530f39d27077947af3ef458988e8ac7b45f8
LaurenCummings <LaurenCummings@users.noreply.github.com>
2025-02-11 21:58:02 -0500

    Added basename prop to BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={movie-project}>
       <App />
     </BrowserRouter>
   </StrictMode>,
