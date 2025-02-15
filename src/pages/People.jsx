261e2d333272b92871b5cbb72b9083095e9c796c
LaurenCummings <LaurenCummings@users.noreply.github.com>
2025-02-14 23:52:27 -0500

    Initial commit

---
+++ src/pages/People.jsx
@@ -0,0 +1,49 @@
+import '../css/People.css';
+import { useState, useEffect } from 'react';
+import { getPopularPeople } from '../services/api';
+
+function People() {
+    const [people, setPeople] = useState([]);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const loadPopularPeople = async () => {
+            try {
+                const popularPeople = await getPopularPeople();
+                setPeople(popularPeople);
+            } catch (err) {
+                console.log(err);
+                setError("Failed to load people...");
+            }
+            finally {
+                setLoading(false);
+            }
+        }
+
+        loadPopularPeople();
+    }, [])
+
+    return (
+        <div className="people">
+
+
+            {error && <div className="error-message">{error}</div>}
+
+            {loading ? (
+                <div className="loading">Loading...</div> 
+            ) : ( 
+                <div className="movies-grid">
+                    {people.map((person) => (
+                        <div>
+                            <img src={`https://image.tmdb.org/t/p/w500${person.profile_path}`} alt={person.name} />
+                            <h3>{person.name}</h3>
+                        </div>
+                    ))}
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default People;
