59c6c54d4eadf94f4f5dd4f5b79f657a456456c9
LaurenCummings <LaurenCummings@users.noreply.github.com>
2025-02-22 22:10:36 -0500

    Added people search

--- src/pages/PopPeople.jsx
+++ src/pages/PopPeople.jsx
@@ -1,8 +1,9 @@
 import '../css/PopPeople.css';
 import { useState, useEffect } from 'react';
-import { getPopularPeople } from '../services/api';
+import { getPopularPeople, searchPeople } from '../services/api';
 
 function PopPeople() {
+    const [searchQuery, setSearchQuery] = useState("");
     const [people, setPeople] = useState([]);
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(true);
@@ -24,9 +25,36 @@ function PopPeople() {
         loadPopularPeople();
     }, [])
 
+    const handleSearch = async (e) => {
+        e.preventDefault();
+        if(!searchQuery.trim()) return
+        if(loading) return
+
+        setLoading(true);
+        try {
+            const searchResults = await searchPeople(searchQuery);
+            setPeople(searchResults);
+            setError(null);
+        } catch (err) {
+            console.log(err);
+            setError("Failed to search people...");
+        } finally {
+            setLoading(false);
+        }
+    };
+
     return (
         <div className="pop-people">
-
+            <form onSubmit={handleSearch} className="search-form">
+                <input 
+                    type="text" 
+                    placeholder="Search for people..." 
+                    className="search-input" 
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                />
+                <button type="submit" className="search-button">Search</button>
+            </form>
 
             {error && <div className="error-message">{error}</div>}
 
