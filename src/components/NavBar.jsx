3932c039e3ef3e34273c53a558c6219369ad6fb0
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-03-11 01:23:02 -0400

    Added hamburger menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,30 @@
 import '../css/NavBar.css';
 import { Link } from 'react-router-dom';
+import { List } from 'phosphor-react';
+import { useState } from 'react';
 
 function NavBar() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen((isOpen) => !isOpen);
+    }
 
     return (
         <nav className="navbar">
             <div className="navbar-brand">
                 <Link to="/">Movie Database</Link>
             </div>
-            <div className="navbar-links">
-                <Link to="/now-showing" className="nav-link">Now Showing</Link>
-                <Link to="/pop-movies" className="nav-link">Popular Movies</Link>
-                <Link to="/pop-people" className="nav-link">Popular People</Link>
-                <Link to="/favorites" className="nav-link">Your Favorite Movies</Link>
+            <div className={`navbar-links ${isOpen ? "menuOpen" : ""}`}>
+                <Link to="/now-showing" className="nav-link" onClick={isOpen ? toggleMenu : null}>Now Showing</Link>
+                <Link to="/pop-movies" className="nav-link" onClick={isOpen ? toggleMenu : null}>Popular Movies</Link>
+                <Link to="/pop-people" className="nav-link" onClick={isOpen ? toggleMenu : null}>Popular People</Link>
+                <Link to="/favorites" className="nav-link" onClick={isOpen ? toggleMenu : null}>Your Favorite Movies</Link>
+            </div>
+            <div>
+                <button className="hamburgerMenu" onClick={toggleMenu}>
+                    <List size={42} />
+                </button>
             </div>
         </nav>
     );
